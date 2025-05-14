2c2f32ba4049f3e70ac6b2f9716131fa24609264
heoyoonah <rnwnsghl_@naver.com>
2025-05-14 12:48:22 +0900

    0514 허윤아: firebase 기본 설정

---
+++ frontend/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { FIREBASE_API_KEY, FIREBASE_PROJECT_ID } from '@env'; 
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: `${FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: `${FIREBASE_PROJECT_ID}.appspot.com`,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
