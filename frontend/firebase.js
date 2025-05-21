4598b7deb1d94dbd4e841aa3d8b714410754bd9a
heoyoonah <rnwnsghl_@naver.com>
2025-05-21 12:04:32 +0900

    0521 허윤아 firebase db 설정

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { FIREBASE_API_KEY, FIREBASE_PROJECT_ID } from '@env'; 
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
@@ -11,3 +12,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
