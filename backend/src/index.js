1966df75e9cad79fea13b5c88a3619ce62f1a781
heoyoonah <rnwnsghl_@naver.com>
2025-05-21 12:27:48 +0900

    0521 허윤아: index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,16 @@
 
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", function (req, res) {
-  res.send("안녕하세요!");
+  res.send("서버 실행 중");
 });
-app.listen(3000, () => console.log("3000포트에서 대기 중..."));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`서버 실행 중: http://localhost:${PORT}`);
+});
